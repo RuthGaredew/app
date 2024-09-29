a8c4ee04e28ffa18fea3e8a4d93f9d5c3671b2dd
RuthGaredew <125625923+RuthGaredew@users.noreply.github.com>
2024-09-29 10:49:39 +0300

    new app

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,21 @@
+export default function HeroSection() { 
+    return (
+        <section id="herosection" className="hero--section"> 
+<div className="hero--section--content--box">
+<div className="hero--section--content">
+    <p className="section--title">Hey, I'm Ruth</p>
+    <h1 className="hero--section--title">
+        <span className="hero--section-title--color">Frontend </span>{""} 
+        <br/>Developmer
+    </h1>
+    <p className="heo--section--description">Ruth Garedew, react front end web developer</p>
+</div>
+<button className="btn btn-primary">Get In Touch</button>
+</div>
+<div className="hero--section--img">
+    <img src="./img/hero_img.png" alt="Hero Section"/>
+</div>
+        </section>
+    );
+    }
+   
