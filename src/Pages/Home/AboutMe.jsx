fc9ddf8648d248e6c48a910c76245760a1ec43ff
RuthGare <163552738+RuthGare@users.noreply.github.com>
2024-10-04 17:11:50 +0300

    update portfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,28 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+          Hello! I'm Ruth Garedew, a Full-Stack Web Developer with experience in a variety of technologies, including React, AngularJS, Next.js, Tailwind CSS, Bootstrap, Express.js, Node.js, MongoDB, and MySQL.
+          </p>
+          <p className="hero--section-description">
+          I am passionate about learning new things and continuously improving my skills. When I'm not coding, you can find me reading novels, watching movies, or traveling with my family.
+          </p>
+          <p className="hero--section-description">
+          As a hardworking team player, I thrive in collaborative environments and easily adapt to new challenges. I love creating innovative solutions and contributing to projects that make a difference.          
+          </p>
+         <p className="hero--section-description">
+          Feel free to explore my portfolio and connect with me!          
+          </p>
+
+        </div>
+      </div>
+    </section>
+  );
+}
